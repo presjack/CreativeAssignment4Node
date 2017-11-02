8b4b78ef1873b18b425959c33d9727085f998449
presjack <prof.prescot@gmail.com>
2017-11-02 21:28:53 +0000

    /getList and /saveList working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('index.html', { root: 'public' });
+});
+
+var listList = {
+  {"store": "Wal-Mart", 
+   "list": ["cups", "bowls", "pastry dishes"] },
+  {"store": "Smith\'s",
+   "list": ["bannanas", "onion"]}
+};
+
+router.get('/getList', function(req, res, next) {
+  res.status(200).json(listList);
+})
+
+router.get('/saveList', function(req, res, next) {
+  listList = JSON.parse(req.query.obj);
 });
 
 module.exports = router;
